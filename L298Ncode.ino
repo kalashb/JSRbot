df5c1ce61e922c7ae364ead296ff3e87fe1ab587
whykalashwhy <kalashbhaiya@gmail.com>
2024-01-20 17:23:56 -0500

    update 2

--- L298Ncode.ino
+++ L298Ncode.ino
@@ -1,16 +1,12 @@
 // Motor A connections
-const int enA = 9;
 const int in1 = 8;
 const int in2 = 7;
 // Motor B connections
-const int enB = 3;
 const int in3 = 5;
 const int in4 = 4;
 
 void setup() {
 	// Set all the motor control pins to outputs
-	pinMode(enA, OUTPUT);
-	pinMode(enB, OUTPUT);
 	pinMode(in1, OUTPUT);
 	pinMode(in2, OUTPUT);
 	pinMode(in3, OUTPUT);
@@ -26,16 +22,10 @@ void setup() {
 void loop() {
 	directionControl();
 	delay(1000);
-	speedControl();
-	delay(1000);
 }
 
 // This function lets you control spinning direction of motors
 void directionControl() {
-	// Set motors to maximum speed
-	// For PWM maximum possible values are 0 to 255
-	analogWrite(enA, 255);
-	analogWrite(enB, 255);
 
 	// Turn on motor A & B
 	digitalWrite(in1, HIGH);
--- UTRAHacksTest1.ino
+++ UTRAHacksTest1.ino
@@ -1,19 +1,52 @@
-const int IN_A0 = A0; // analog input
-const int IN_D0 = 8; // digital input
+const int IR1_A = A0; // analog input IR left
+const int IR1_D = 8; // digital input IR left
+double A0_Voltage;
+
+const int IR2_A = A1; // analog input IR right
+const int IR2_D = 9; // digital input IR right
+
+const int M_IN1 = 2; // left motor IN1
+const int M_IN2 = 3; // left motor IN2
+const int M_IN3 = 4; // right motor IN2
+const int M_IN4 = 5; // right motor IN1
+
+#define White 1
+#define Black 0
+
 void setup() {
-Serial.begin(9600); //clockspeed 9600
-pinMode (IN_A0, INPUT); 
-pinMode (IN_D0, INPUT);
+Serial.begin(9600);
+pinMode (IR1_A, INPUT);
+pinMode (IR1_D, INPUT);
 
 }
-int value_A0;
-bool value_D0;
+int valueIR1_A;
+bool valueIR1_D;
 void loop() {
-value_A0 = analogRead(IN_A0); // reads the analog input from the IR distance sensor (0, 5)(range/bitrate = size of each bit)(size of each bit(analog) * 5/1024 = voltage)
-value_D0 = digitalRead(IN_D0);// reads the digital input from the IR distance sensor {0, 5}
+valueIR1_A = analogRead(IR1_A); // reads the analog input from the IR distance sensor
+valueIR1_D = digitalRead(IR1_D);// reads the digital input from the IR distance sensor
+A0_Voltage = (double)(valueIR1_A * (double) 5 / 1024);
+
+if (IR1_D == Black && IR2_D == White){
+digitalWrite(M_IN1, 0);
+digitalWrite(M_IN2, 1);
+digitalWrite(M_IN3, 0);
+digitalWrite(M_IN4, 1);
+}
+else if (IR1_D == White && IR2_D == Black){
+digitalWrite(M_IN1, 1);
+digitalWrite(M_IN2, 0);
+digitalWrite(M_IN3, 1);
+digitalWrite(M_IN4, 0);
+}
+else if (IR1_D == White && IR2_D == White){
+digitalWrite(M_IN1, 1);
+digitalWrite(M_IN2, 0);
+digitalWrite(M_IN3, 0);
+digitalWrite(M_IN4, 1);
+}
 Serial.print(" Analog = "); 
-Serial.print(value_A0);
+Serial.print(A0_Voltage);
 Serial.print("\t Digital ="); 
-Serial.println(value_D0);
-delay(100); //milliseconds
+Serial.println(valueIR1_D);
+delay(100);
 }
